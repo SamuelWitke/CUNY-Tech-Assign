7b4272e34a12ed7983e51debdb2b28c034f66e7f
Samuel Witke <witkesam@gmail.com>
2017-08-02 18:57:02 -0400

    Solved:   deep-comparison.js

---
+++ Chapter-4/deep-comparison.js
@@ -0,0 +1,20 @@
+/*
+	Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects with the same properties whose values are also equal when compared with a recursive call to deepEqual.
+*/
+function deepEqual(obj1,obj2) {
+	if(typeof obj1 != "object" && obj1 != null) {
+		return obj1 == obj2;
+	}
+	for(var property in obj1) {
+		 if (!obj2.hasOwnProperty(property)) {
+			return false;
+		 }
+		 return deepEqual(obj1[property],obj2[property]);
+	}	
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+
